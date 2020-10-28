5444b7c162e1e7377eacafc767ee594ad3fc6af2
wbglaeser <ben.glaeser@tuta.io>
2020-10-28 16:45:10 +0100

    fix minor bug in compare triplets script

--- code/compare_triplets.c
+++ code/compare_triplets.c
@@ -14,40 +14,12 @@ char* ltrim(char*);
 char* rtrim(char*);
 char** split_string(char*);
 
-// Complete the compareTriplets function below.
-
-/*
- * To return the integer array from the function, you should:
- *     - Store the size of the array to be returned in the result_count variable
- *     - Allocate the array statically or dynamically
- *
- * For example,
- * int* return_integer_array_using_static_allocation(int* result_count) {
- *     *result_count = 5;
- *
- *     static int a[5] = {1, 2, 3, 4, 5};
- *
- *     return a;
- * }
- *
- * int* return_integer_array_using_dynamic_allocation(int* result_count) {
- *     *result_count = 5;
- *
- *     int *a = malloc(5 * sizeof(int));
- *
- *     for (int i = 0; i < 5; i++) {
- *         *(a + i) = i + 1;
- *     }
- *
- *     return a;
- * }
- *
- */
 int* compareTriplets(int a_count, int* a, int b_count, int* b, int* result_count) {
 
     for (int i = 0; i < 3; i++) {
         if (*(a+i) == *(b+i)) {
-          continue;
+          a_count -= 1;
+          b_count -= 1;
         } else if (*(a+i) > *(b+i)) {
           b_count -= 1;
         } else {
