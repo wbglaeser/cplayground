1bb2a6819287a592e09236f11ce28fb93e08a549
wbglaeser <ben.glaeser@tuta.io>
2020-10-30 17:06:37 +0100

    fix typo in staircase

--- code/staircase.c
+++ code/staircase.c
@@ -21,11 +21,11 @@ void staircase(int n) {
         strcpy(layer, "");
 
         for (int j=0; j < n-i-1; j++) {
-            strcat(layer, " ");
+            strcat(layer, space);
         }
 
         for (int k=0; k < i+1; k++) {
-            strcat(layer, "#");
+            strcat(layer, step);
         }
 
         printf("%s\n", layer);
