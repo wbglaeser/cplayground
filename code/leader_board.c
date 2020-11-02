b7284a1b6b8e82deba127922267de22989513e42
wbglaeser <ben.glaeser@tuta.io>
2020-11-02 16:11:06 +0100

    add leader board challenge - speed up only possible after check in forums

---
+++ code/leader_board.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// variables needed throughout the code
+int i, n, m, score;
+
+// the stack is implemented using an array
+// and a variable storing the index of the top element
+// note: global variables are automatically intialized to 0,
+//       so top is 0 and the elements of the stack are 0
+int top, stack[200002];
+
+int main() {
+    // read n - the number of players on the leaderboard
+    scanf("%d", &n);
+
+
+    for (i = 0; i < n; ++i) {
+
+        scanf("%d", &stack[top+1]);
+
+        if (stack[top+1] != stack[top]) ++top;
+    }
+
+    // read m - the number of levels Alice beats
+    scanf("%d", &m);
+
+    // read Alice's scores and output the corresponding ranks
+    for (i = 0; i < m; ++i) {
+        // read a score
+        scanf("%d", &score);
+        while (top && score >= stack[top]) --top;
+
+        // output the current rank
+        printf("%d\n", top+1);
+    }
+    return 0;
+}
