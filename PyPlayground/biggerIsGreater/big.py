cc0e0b729c202e9b21b1afa9fcf2c5ec48851f30
wbglaeser <ben.glaeser@tuta.io>
2021-01-25 11:47:28 +0100

    biggerIsGreaterFinish

--- PyPlayground/biggerIsGreater/big.py
+++ PyPlayground/biggerIsGreater/big.py
@@ -1,18 +1,26 @@
-s = "dkhc"
+s = "fedcbabcd"
+from bisect import bisect
 
-s = list(s)
-
-# first increase
-
-for start_id in range(len(s))[::-1]:
-    stop = 0
-    for val in range(start_id)[::-1]:
-        _s = s.copy()
-        _s[start_id], _s[val] = _s[val], _s[start_id]
-        if "".join(_s) > "".join(s):
-            s = _s
-            stop = 1
+def run(s):
+    f = "no answer"
+    s = s.rstrip("\n")
+    for idx in range(1, len(s)):
+        if any(x > s[len(s)-idx-1] for x in s[len(s)-idx:]):
+            swx = bisect(sorted(s[len(s)-idx:]), s[len(s)-idx-1])
+            end = sorted(s[len(s)-idx:])[:]
+            end.pop(swx)
+            f = s[:len(s)-idx-1] + sorted(s[len(s)-idx:])[swx] +  "".join(sorted(end + list(s[len(s)-idx-1]))) + "\n"
             break
-    if stop == 1:
-        break
-print("".join(s))
+    return f
+
+with open("inputtestcase") as f:
+    lines = f.readlines()
+
+with open("inputtestans") as f:
+    ans = f.readlines()
+
+for idx, line in enumerate(lines):
+    if run(line) != ans[idx]:
+        print(repr(run(line)))
+        print(repr(ans[idx]))
+
